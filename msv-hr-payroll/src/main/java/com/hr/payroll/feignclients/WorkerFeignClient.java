bc03b2a1f33f61391d9ea3e20c73708b7ac7d723
Weliton Martins <weliton.martins@valemobi.com.br>
2021-10-31 00:27:06 -0300

    feat: config eureka client

--- msv-hr-payroll/src/main/java/com/hr/payroll/feignclients/WorkerFeignClient.java
+++ msv-hr-payroll/src/main/java/com/hr/payroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "msv-hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "msv-hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
     @GetMapping(value = "/{id}")
--- msv-hr-worker/src/main/java/com/hr/worker/controller/WorkerController.java
+++ msv-hr-worker/src/main/java/com/hr/worker/controller/WorkerController.java
@@ -3,6 +3,9 @@ package com.hr.worker.controller;
 import com.hr.worker.entity.Worker;
 import com.hr.worker.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/workers")
+@Slf4j
 public class WorkerController {
 
     private final WorkerRepository workerRepository;
+
+    private final Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -25,6 +31,9 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        log.info("PORT = "+ env.getProperty("local.server.port"));
+
         return ResponseEntity.ok(workerRepository.findById(id).orElse(null));
     }
 }
